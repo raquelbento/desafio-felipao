4283052ee2255fc9afe9fffa61fae45623ba387c
raquelbento <rakelbento27@gmail.com>
2023-11-27 11:11:46 -0300

    Update index.js
    desafio de logica

---
+++ index.js
@@ -0,0 +1,36 @@
+let nomeHeroi = "Raquel Bento"
+let quantXp = 9000
+
+if (quantXp <= 1000) {
+    heroClass = "Ferro";
+} 
+
+if (quantXp > 1000 && quantXp < 2001) {
+    heroiNivel = "Bronze";
+} 
+
+if (quantXp > 2000 && quantXp < 6001) {
+    heroiNivel = "Prata";
+}
+
+if (quantXp > 6000 && quantXp < 7001) {
+    heroiNivel = "Ouro";
+} 
+
+if (quantXp > 7000 && quantXp < 8001) {
+    heroiNivel = "Platina";
+} 
+
+if (quantXp > 8000 && quantXp < 9001) {
+    heroiNivel = "Ascendente";
+} 
+
+if (quantXp > 9000 && quantXp < 10001) {
+    heroiNivel = "Imortal";
+} 
+
+if (quantXp >= 10001) {
+    heroiNivel = "Radiante";
+} 
+
+console.log ("O Herói de nome " + nomeHeroi + " está no nível  " + heroiNivel)
